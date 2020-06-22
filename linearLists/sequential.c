c7e638466434979854b8767ebcc13e4d3009d961
Danilo Moura <danilolmoura@gmail.com>
2020-06-22 01:22:25 -0300

    Add ordered insert, binary search and some examples

--- linearLists/sequential.c
+++ linearLists/sequential.c
@@ -3,10 +3,10 @@
 
 #define MAXELEMENTS 50
 
-typedef int KEY;
+typedef int VALUE;
 
 typedef struct{
-	KEY key;
+	VALUE value;
 } ELEMENT;
 
 typedef struct{
@@ -41,30 +41,59 @@ void printList(LIST* l){
  * @param l pointer to the LIST
  ******************************************************************************/
 	int i;
-	printf("List: \" ");
+	printf("List: \"");
 	for (i=0; i < l->totalElements; i++){
-		printf("%i ", l->ARR[i].key);
+		printf("%i ", l->ARR[i].value);
 	}
-	printf("\n");
+	printf("\"\n");
 }
 
-int linearSearch(LIST* l, KEY key){
+int linearSearch(LIST* l, VALUE value){
 /***************************************************************************//**
- * Finds position of a number in a LIST
+ * Finds position of a value in a LIST
  *
  * @param LIST pointer to the LIST
- * @param KEY value that we want to find inside the LIST
+ * @param VALUE value that we want to find inside the LIST
  *
- * @return int position of the key in the list
+ * @return int position of the value in the list
  ******************************************************************************/
 	int i = 0;
 
 	while(i < l->totalElements){
-		if(key == l->ARR[i].key) return i;
+		if(value == l->ARR[i].value) return i;
 		else i++;
 	}
 
 	return i;
+}
+
+int binarySearch(LIST* l, VALUE value){
+/***************************************************************************//**
+ * Finds position of a value in an orderred list
+ *
+ * @param LIST pointer to the LIST
+ * @param VALUE value that we want to find inside the LIST
+ *
+ * @return int position of the value in the list
+ ******************************************************************************/
+	int left, right, mid;
+	left = 0;
+	right = l->totalElements-1;
+
+	while(left <= right){
+		mid = ((left + right) / 2);
+
+		if(l->ARR[mid].value == value){
+			return mid;
+		} else{
+			if(l->ARR[mid].value < value){
+				left = mid + 1;
+			} else{
+				right = mid - 1;
+			}
+		}
+	}
+	return -1;
 }
 
 bool insertElement(LIST* l, ELEMENT el, int pos){
@@ -92,18 +121,41 @@ bool insertElement(LIST* l, ELEMENT el, int pos){
 	return true;
 }
 
-bool deleteElement(LIST* l, KEY key){
+bool insertOrderedElement(LIST* l, ELEMENT el){
+/***************************************************************************//**
+ * Insert an element in the list and keep its ordering
+ *
+ * @param LIST* pointer to the LIST
+ * @param Element that will be inserted in the List
+ *
+ * @return bool true if element was inserted and false if element wasn't inserted
+ ******************************************************************************/
+	if(l->totalElements >= MAXELEMENTS) return false;
+
+	int pos = l->totalElements;
+
+	while(pos > 0 && l->ARR[pos-1].value > el.value){
+		l->ARR[pos] = l->ARR[pos-1];
+		pos--;
+	}
+
+	l->ARR[pos] = el;
+	l->totalElements++;
+	return true;
+}
+
+bool deleteElement(LIST* l, VALUE value){
 /***************************************************************************//**
  * Delete an element in a specific position of a List
  *
  * @param LIST* pointer to the LIST
- * @param Element that will be deleted in the List
+ * @param VALUE value that will be deleted in the List
  *
  * @return bool true if element was deleted and false if element wasn't deleted
  ******************************************************************************/
 	int pos, j;
 
-	pos = linearSearch(l, key);
+	pos = binarySearch(l, value);
 	if(pos == -1){
 		return false;	
 	}
@@ -129,8 +181,40 @@ void resetList(LIST* l){
 
 int main(void) {
 	LIST l = (LIST) {};
+	VALUE value1 = 333;
+	VALUE value2 = 642;
+	VALUE value3 = 33;
+	VALUE value4 = 5;
+	VALUE value5 = 55;
+
+	ELEMENT e1 = (ELEMENT) {value1};
+	ELEMENT e2 = (ELEMENT) {value2};
+	ELEMENT e3 = (ELEMENT) {value3};
+	ELEMENT e4 = (ELEMENT) {value4};
+	ELEMENT e5 = (ELEMENT) {value5};
+
 	initList(&l);
+	insertOrderedElement(&l, e5);
+	insertOrderedElement(&l, e4);
+	insertOrderedElement(&l, e3);
+	insertOrderedElement(&l, e2);
+	insertOrderedElement(&l, e1);
+
 	int totalElements = countElements(&l);
-	printf("%i \n", totalElements);
+	printf("Total elements: %i \n", totalElements);
+
+	int pos1 = binarySearch(&l, value1);
+	int pos2 = binarySearch(&l, value2);
+	int pos3 = binarySearch(&l, value3);
+	int pos4 = binarySearch(&l, value4);
+	int pos5 = binarySearch(&l, value5);
+
+	printList(&l);
+	printf("Position of %i in list is %i \n", value1, pos1);
+	printf("Position of %i in list is %i \n", value2, pos2);
+	printf("Position of %i in list is %i \n", value3, pos3);
+	printf("Position of %i in list is %i \n", value4, pos4);
+	printf("Position of %i in list is %i \n", value5, pos5);
+
 }
 
